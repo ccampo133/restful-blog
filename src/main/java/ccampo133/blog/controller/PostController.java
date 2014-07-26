392004b8abbf25023f875edb071b5c68ef1faaa7
Chris Campo <christopher.campo@aspect.com>
2014-07-26 01:11:38 -0400

    TODO: fix PUT

--- src/main/java/ccampo133/blog/controller/PostController.java
+++ src/main/java/ccampo133/blog/controller/PostController.java
@@ -40,6 +40,7 @@ public class PostController {
     }
 
     // UPDATE
+    // TODO: scrub this - needs to be idempotent. Replace with patch?
     @RequestMapping(value = "/api/posts/{id}", method = RequestMethod.PUT)
     public ResponseEntity<Void> updatePost(@PathVariable("id") final long id, @RequestBody final Post post,
             final Principal principal) throws PostNotFoundException {
