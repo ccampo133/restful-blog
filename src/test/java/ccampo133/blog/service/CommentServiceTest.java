6b0abcff2650ab3916c9d0f161a44608c0306ea9
Chris Campo <christopher.campo@aspect.com>
2014-08-02 00:11:06 -0400

    Finished comment service tests.

--- src/test/java/ccampo133/blog/service/CommentServiceTest.java
+++ src/test/java/ccampo133/blog/service/CommentServiceTest.java
@@ -2,11 +2,13 @@ package ccampo133.blog.service;
 
 import ccampo133.blog.domain.Comment;
 import ccampo133.blog.domain.Post;
+import ccampo133.blog.exception.CommentNotFoundException;
 import ccampo133.blog.repository.CommentRepository;
 import mockit.*;
 import mockit.integration.junit4.JMockit;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import java.util.Date;
 import java.util.List;
@@ -63,18 +65,74 @@ public class CommentServiceTest {
         assertEquals("Result does not match expected", comments, result);
     }
 
-    @Test
-    public void testGetCommentById() {
-        // TODO
+    @Test(expected = CommentNotFoundException.class)
+    public void getCommentByIdThrowsCommentNotFoundException() throws CommentNotFoundException {
+        final long id = 14252213;
+        new Expectations() {{
+            commentRepository.findOne(id);
+            result = null;
+        }};
+
+        commentService.getCommentById(id);
     }
 
     @Test
-    public void testUpdateCommentById() {
-        // TODO
+    public void getCommentByIdReturnsComment(@Mocked final Comment comment) throws CommentNotFoundException {
+        final long id = 14252213;
+        new Expectations() {{
+            commentRepository.findOne(id);
+            result = comment;
+        }};
+
+        Comment result = commentService.getCommentById(id);
+        assertEquals("Result does not match expected.", comment, result);
+    }
+
+    @Test(expected = CommentNotFoundException.class)
+    public void updateCommentByIdThrowsCommentNotFoundException(@Mocked final Comment comment)
+            throws CommentNotFoundException {
+        final long id = 9823529;
+        new Expectations() {{
+            commentRepository.findOne(id);
+            result = null;
+        }};
+
+        commentService.updateCommentById(id, comment);
     }
 
     @Test
-    public void testDeleteCommentById() {
-        // TODO
+    public void updateCommentByIdSetsBodyAndSaves(@Mocked final Comment comment) throws CommentNotFoundException {
+        final long id = 82342384;
+        new Expectations() {{
+            commentRepository.findOne(id);
+            result = comment;
+
+            comment.setBody(comment.getBody());
+            commentRepository.save(comment);
+        }};
+
+        commentService.updateCommentById(id, comment);
+    }
+
+    @Test(expected = CommentNotFoundException.class)
+    public void deleteCommentByIdThrowsCommentNotFoundException(@Mocked final EmptyResultDataAccessException ex)
+            throws CommentNotFoundException {
+        final long id = 90204;
+        new Expectations() {{
+            commentRepository.delete(id);
+            result = ex;
+        }};
+
+        commentService.deleteCommentById(id);
+    }
+
+    @Test
+    public void deleteCommentByIdIsSuccessful() throws CommentNotFoundException {
+        final long id = 242;
+        new Expectations() {{
+            commentRepository.delete(id);
+        }};
+
+        commentService.deleteCommentById(id);
     }
 }
