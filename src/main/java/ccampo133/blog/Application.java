b13516a689805dc5e3307a3601ffdc4b2d639114
Chris Campo <christopher.campo@aspect.com>
2014-08-14 14:13:02 -0400

    Changed "body" to "content".

--- src/main/java/ccampo133/blog/Application.java
+++ src/main/java/ccampo133/blog/Application.java
@@ -3,11 +3,11 @@ package ccampo133.blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
 
-@Configuration
-@EnableAutoConfiguration
 @ComponentScan
+@EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
+@EnableAutoConfiguration
 public class Application {
 
     public static void main(final String[] args) {
